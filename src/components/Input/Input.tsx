474e914c5a19aab55c5e61854f13b154153b1e03
Kirill Starodubsev <colereteps@gmail.com>
2023-01-28 16:10:04 +0400

    added input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,25 +1,31 @@
 import styles from './Input.module.scss';
 import { IInput } from './Input.props';
-import { FC, useState } from 'react';
+import { FC, useState, useRef, useEffect } from 'react';
 
 export const Input: FC<{ change: (value: string) => void }> = ({ change }) => {
 	const [message, setMessage] = useState('');
-
+	const textareaRef = useRef(null);
 	const submitMessage = () => {
 		change(message);
 		setMessage('');
 	};
+
 	return (
 		<div className={styles.container}>
-			<input
-				placeholder='text'
+			<textarea
+				placeholder='...hello my friend'
 				value={message}
 				onChange={(e) => {
 					setMessage(e.target.value);
 				}}
 				className={styles.input}
-			></input>
-			<button type='submit' onClick={() => submitMessage()}>
+				ref={textareaRef}
+			></textarea>
+			<button
+				type='submit'
+				onClick={() => submitMessage()}
+				className={styles.button}
+			>
 				Send
 			</button>
 		</div>
--- src/components/MainField/MainField.tsx
+++ src/components/MainField/MainField.tsx
@@ -1,15 +1,16 @@
 import { IMainField } from './MainField.props';
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import styles from './MainField.module.scss';
 import img from '../../assets/UI/button/button-avatar-blue2.png';
 import { Message } from '../Message/Message';
 import { Input } from '../Input/Input';
 const MainField: FC<IMainField> = ({ ...props }) => {
-	const [message, setMessage] = useState('');
+	const [message, setMessage] = useState<string>('');
+
 	const getMessage = (value: string) => {
 		setMessage(value);
-		console.log(message);
 	};
+
 	return (
 		<div {...props}>
 			<div className={styles.container}>
@@ -17,10 +18,7 @@ const MainField: FC<IMainField> = ({ ...props }) => {
 					<h6 className={styles['title-message']}>Your Messages</h6>
 					<h5 className={styles['title-name-friend']}>Elvira Stray</h5>
 					<div className={styles['block-messages']}>
-						<Message user='user' text='Hello Bro' />
-						<Message user='friend' text='Hello' />
-						<Message user='friend' text='Hello' />
-						<Message user='user' text={message} />
+						{message && <Message user={'user'} text={message} />}
 					</div>
 					<div className={styles['input-field']}>
 						<Input change={getMessage}></Input>
